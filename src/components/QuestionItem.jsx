5ee15ffaee4a52898d3ce4024f2212728e4c72d2
Sri Lakshmi Polavarapu <srilakshmi.polavarapu01@gmail.com>
2025-11-26 15:29:33 -0800

    Add a checkbox to track the solved questions and highlighted difficulty tags

--- src/components/QuestionItem.jsx
+++ src/components/QuestionItem.jsx
@@ -1,7 +1,17 @@
 import {useNavigate} from "react-router-dom";
+import { useState } from "react";
 
-export default function QuestionItem({question}){
+export default function QuestionItem({question, isSolved}){
     const navigate = useNavigate();
+    const [checked, setChecked] = useState(isSolved);
+
+    //Set color
+    const difficultyColor = {
+        Easy: "green",                                                                                                                                                                                                                                                                                                    
+        Medium: "orange",
+        Hard: "red"
+    };
+    
 
     return (
         <div
@@ -11,11 +21,40 @@ export default function QuestionItem({question}){
             padding:"15px",
             margin:"10px 0",
             borderRadius:"8px",
-            cursor:"pointer"
+            cursor:"pointer",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between"
+          }}
+
+    >
+      {/* LEFT SIDE: Checkbox + Question Title */}
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+          onClick={(e) => e.stopPropagation()} 
+          style={{ marginRight: "10px" }}
+        />
+
+        <h3 style={{ margin: 0 }}>{question.title}</h3>
+      </div>
+
+      {/* RIGHT SIDE: Difficulty Tag */}
+      <span
+        style={{
+          background: difficultyColor[question.difficulty],
+          color: "white",
+          padding: "5px 10px",
+          borderRadius: "10px",
+          fontSize: "0.9rem",
+          marginLeft: "10px",
+          whiteSpace: "nowrap"
         }}
-        >
-            <h3>{question.title}</h3>
-            <p>Difficulty:{question.difficulty}</p>
-        </div>
-    );
+      >
+        {question.difficulty}
+      </span>
+    </div>
+  );
 }
